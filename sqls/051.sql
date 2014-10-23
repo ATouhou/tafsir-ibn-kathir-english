5562c5b3e929f3183c818e7d7ad507d1ac777043
mkhan3189 <mkhan3189@gmail.com>
2014-10-24 10:21:15 +1100

    51:1-7

--- sqls/051.sql
+++ sqls/051.sql
@@ -1,23 +1,57 @@
--- http://muflihun.com/quran/print?ref=1&fs=32:22&tra=1&tra2=0&tra3=0&tra4=0&tid=8100&taf=1&tafid=8168&tafdir=lr&tafclass=script-uthmani&tri=0&arc=f6eede&qscrid=1&scr=5&s=51&v=1-3&style=2&nologo&fnclr&main&bism&title&title-mean&title&title-mean&fs=32:22&bism&fnclr
+-- http://muflihun.com/quran/print?ref=1&fs=32:22&tra=1&tra2=0&tra3=0&tra4=0&tid=8100&taf=1&tafid=1000&tafdir=lr&tafclass=script-uthmani&tri=0&arc=f6eede&qscrid=1&scr=5&s=51&v=1-3&style=2&nologo&fnclr&main&bism&title&title-mean&title&title-mean&fs=32:22&bism&fnclr
 -- Page 252 in Darussalam Tafsir Ibn Kathir
 -- http://books.google.com.au/books?id=kN3V_Cs6gswC&lpg=PA279&ots=Hdcj6umRNR&dq=muwatta%201%3A78&pg=PA252#v=onepage&q=muwatta%201:78&f=false
 
-DELETE FROM `Quran` WHERE `DatabaseID` = 8168 AND `SuraID` = 51;
+DELETE FROM `Quran` WHERE `DatabaseID` = 1000 AND `SuraID` = 51;
 INSERT INTO `Quran`
   (`DatabaseID`, `SuraID`, `VerseID`, `AyahText`)
 VALUES
-(8168, 51, 1, '
-The Commander of the faithful, Ali ibn Abi Talib may Allah be pleased with him, ascended the Minbar in Kufah and declared, "Any Ayah in the Book of Allah the Exalted and any Sunnah from Allah\'s Messenger ﷺ you ask me about toda, I will explain them." Ibn Al-Kawwa stood up and said, "O Leader of the faithful! What is the meaning of Allah\'s statement,
+(1000, 51, 1, '
+The Commander of the faithful, Ali ibn Abi Talib may Allah be pleased with him, ascended the Minbar in Kufah and declared, "Any Ayah in the Book of Allah the Exalted and any Sunnah from Allah\'s Messenger ﷺ you ask me about today, I will explain them." Ibn Al-Kawwa stood up and said, "O Leader of the faithful! What is the meaning of Allah\'s statement,
 <span class="taf-ref-3 taf-arb taf-qur">(۱) والذاريات ذروا </span>
 <span class="taf-ref-3 taf-tr taf-tr-qur">By the scattering Dhariyat</span>?" and Ali said, "The wind."
+<span class="taf-ay-end"></span>
 '),
-(8168, 51, 2, '
+(1000, 51, 2, '
 The man asked, 
 <span class="taf-ref-3 taf-arb taf-qur">(۲) فالحاملات وقرا</span>
 <span class="taf-ref-3 taf-tr taf-tr-qur">And laden Hamilat</span>?" and Ali said, "The clouds."
+<span class="taf-ay-end"></span>
 '),
-(8168, 51, 3, '
+(1000, 51, 3, '
 The man asked, 
 <span class="taf-ref-3 taf-arb taf-qur">(۳) فالجاريات يسرا</span>
 <span class="taf-ref-3 taf-tr taf-tr-qur">And the steady Jariyaat</span>?" and Ali said, "The ships."
+<span class="taf-ay-end"></span>
+'),
+(1000, 51, 4, '
+The man asked, 
+<span class="taf-ref-3 taf-arb taf-qur">(۴)  فالمقسمات أمرا</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">And the distributors of command</span>?" and Ali said, it refers to "The angels."
+<br>Some scholars said that <span class="taf-tri">Al-Jariyat Yusra</span> refers to the stars that float in their orbits with ease. This would mean that the things mentioned were ascendant in their order, beginning with the lower, then mentioning the higher one after that, etc. The winds being the clouds, the stars are above them and the angels who distribute by Allah\'s order are above that, and they descend with Allah\'s legislative orders and the decrees He determines.
+<span class="taf-ay-end"></span>
+'),
+(1000, 51, 5, '
+These Ayat contain a vow from Allah that Resurrection shall come to pass. Allah\'s statement
+<span class="taf-ref-3 taf-arb taf-qur">(۵) إنما توعدون لصادق</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Verily, that which you are promised is surely true.</span>, it is a truthful promise
+<span class="taf-ay-end"></span>
+'),
+(1000, 51, 6, '
+<span class="taf-ref-3 taf-arb taf-qur">وإن الدين</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">And verily, Ad-Din</span> the Recompense,
+<span class="taf-ref-3 taf-arb taf-qur">لواقع</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">will occur</span>, it will surely come to pass.
+<span class="taf-ay-end"></span>
+'),
+(1000, 51, 7, '
+Then Allah the Exalted said,
+<span class="taf-ref-3 taf-arb taf-qur">(۷) والسماء ذات الحبك</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">By the heaven full of Hubuk</span> Ibn \'Abbas said; "Full of beauty, grace, magnificence and perfection." Mujahid, \'Ikrimah, Sa\'d bin Jubayr, Abu Malik, Abu Salih, As Suddi, Qatadah, \'Atiyah Al-\'Awfi, Ar-Rabi\' bin Anas and other said similarly. Ad-Dahhak, Al-Minhal bin \'Amr and others said, "The meandering of the water, sand and plants when the wind passes over them; carving paths out of them, that is the Hubuk."
+<br/>
+All of these sayings return to the same meaning, that of beauty and complexity. The sky is high above us, clear yet think, firmly structured, spacious and graceful, beautified with stars such as the sun and orbiting planets such as the moon and the planets of the solar system.
+<span class="taf-ay-end"></span>
 ')
+
+
+
