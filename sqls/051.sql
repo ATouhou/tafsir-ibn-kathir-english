1496dd75aabe4e7508a0a92d4750d4ed41ed2f0b
Majid Khan <mkhan3189@gmail.com>
2014-11-14 23:54:00 +1100

    51:15-21

--- sqls/051.sql
+++ sqls/051.sql
@@ -27,7 +27,7 @@ The man asked,
 (1000, 51, 4, '
 The man asked, 
 <span class="taf-ref-3 taf-arb taf-qur">(۴)  فالمقسمات أمرا</span>
-<span class="taf-ref-3 taf-tr taf-tr-qur">And the distributors of command</span>?" and Ali said, it refers to "The angels."<span class="taf-foot"><span class="n">1</span><span class="t">At-Tabari 22:389-392 and `Abdur-Razzaq 3:41</span></span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">And the distributors of command</span>?" and Ali said, it refers to "The angels."<span class="taf-foot"><span class="t">At-Tabari 22:389-392<br/>`Abdur-Razzaq 3:41</span></span>
 <br>Some scholars said that <span class="taf-tri">Al-Jariyat Yusra</span> refers to the stars that float in their orbits with ease. This would mean that the things mentioned were ascendant in their order, beginning with the lower, then mentioning the higher one after that, etc. The winds being the clouds, the stars are above them and the angels who distribute by Allah\'s order are above that, and they descend with Allah\'s legislative orders and the decrees He determines.
 <span class="taf-ay-end"></span>
 '),
@@ -47,7 +47,7 @@ These Ayat contain a vow from Allah that Resurrection shall come to pass. Allah\
 (1000, 51, 7, '
 Then Allah the Exalted said,
 <span class="taf-ref-3 taf-arb taf-qur">(۷) والسماء ذات الحبك</span>
-<span class="taf-ref-3 taf-tr taf-tr-qur">By the heaven full of Hubuk</span> Ibn \`Abbas said; "Full of beauty, grace, magnificence and perfection."<span class="taf-foot"><span class="n">2</span><span class="t">At-Tabari 22:395-396</span></span> Mujahid, \`Ikrimah, Sa\'d bin Jubayr, Abu Malik, Abu Salih, As Suddi, Qatadah, \`Atiyah Al-\`Awfi, Ar-Rabi\` bin Anas and other said similarly.<span class="taf-foot"><span class="n">3</span><span class="t">At-Tabari 22:396-397</span></span> Ad-Dahhak, Al-Minhal bin \`Amr and others said, "The meandering of the water, sand and plants when the wind passes over them; carving paths out of them, that is the Hubuk."
+<span class="taf-ref-3 taf-tr taf-tr-qur">By the heaven full of Hubuk</span> Ibn \`Abbas said; "Full of beauty, grace, magnificence and perfection."<span class="taf-foot"><span class="t">At-Tabari 22:395-396</span></span> Mujahid, \`Ikrimah, Sa\'d bin Jubayr, Abu Malik, Abu Salih, As Suddi, Qatadah, \`Atiyah Al-\`Awfi, Ar-Rabi\` bin Anas and other said similarly.<span class="taf-foot"><span class="t">At-Tabari 22:396-397</span></span> Ad-Dahhak, Al-Minhal bin \`Amr and others said, "The meandering of the water, sand and plants when the wind passes over them; carving paths out of them, that is the Hubuk."
 <br/>
 All of these sayings return to the same meaning, that of beauty and complexity. The sky is high above us, clear yet think, firmly structured, spacious and graceful, beautified with stars such as the sun and orbiting planets such as the moon and the planets of the solar system.
 <span class="taf-ay-end"></span>
@@ -55,7 +55,7 @@ All of these sayings return to the same meaning, that of beauty and complexity. 
 (1000, 51, 8, '
 Allah the Exalted said,
 <span class="taf-ref-3 taf-arb taf-qur">(۸) إنكم لفي قول مختلف</span>
-<span class="taf-ref-3 taf-tr taf-tr-qur">Certainly, you have different ideas</span> Allah says, \'you disbelievers who deny the Messengers have different and confused opinions that do not connect or conform to each other.\' Qatadah commented on the Ayah, "You have different ideas about that Qur\'an. Some of you agree that it is true while some others deny this fact."<span class="taf-foot"><span class="n">4</span><span class="t">`Abdur-Razzaq 4:242</span></span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Certainly, you have different ideas</span> Allah says, \'you disbelievers who deny the Messengers have different and confused opinions that do not connect or conform to each other.\' Qatadah commented on the Ayah, "You have different ideas about that Qur\'an. Some of you agree that it is true while some others deny this fact."<span class="taf-foot"><span class="t">`Abdur-Razzaq 4:242</span></span>
 <span class="taf-ay-end"></span>
 '),
 (1000, 51, 9, '
@@ -77,17 +77,17 @@ Allah said,
 <span class="taf-ref-3 taf-arb taf-qur">(۱۷) قتل الإنسان ما أكفره</span>
 <span class="taf-ref-3 taf-tr taf-tr-qur">Be cursed man! How ungrateful he is! (<a class="footnote-qref-link">80:17</a>)</span>
 <br/>
-Al-Kharrasun are those who claim that they will never be brought back to life, doubting the coming of Resurrection."<span class="taf-foot"><span class="n">5</span><span class="t">At-Tabari 22:400</span></span>
+Al-Kharrasun are those who claim that they will never be brought back to life, doubting the coming of Resurrection."<span class="taf-foot"><span class="t">At-Tabari 22:400</span></span>
 <br/>
 \`Ali bin Abi Talhah reported from Ibn \`Abbas;
 <span class="taf-ref-3 taf-arb taf-qur">(۱۰) قتل الخراصون</span>
-<span class="taf-ref-3 taf-tr taf-tr-qur">Cursed be Al-Kharrasun</span>  "Cursed be the doubters."<span class="taf-foot"><span class="n">6</span><span class="t">At-Tabari 22:399</span></span> Mu\`adh said similarly, may Allah be pleased with him. During one of his speeches he said, "Destroyed be the doubters." Qatadah said, "Al-Kharrasun are the pople of doubt and suspicion."<span class="taf-foot"><span class="n">7</span><span class="t">At-Tabari 22:400</span></span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Cursed be Al-Kharrasun</span>  "Cursed be the doubters."<span class="taf-foot"><span class="t">At-Tabari 22:399</span></span> Mu\`adh said similarly, may Allah be pleased with him. During one of his speeches he said, "Destroyed be the doubters." Qatadah said, "Al-Kharrasun are the pople of doubt and suspicion."<span class="taf-foot"><span class="t">At-Tabari 22:400</span></span>
 <span class="taf-ay-end"></span>
 '),
 (1000, 51, 11, '
 Allah said,
 <span class="taf-ref-3 taf-arb taf-qur">(۱۱) الذين هم في غمرة ساهون</span>
-<span class="taf-ref-3 taf-tr taf-tr-qur">Who are under a cover of Sahun</span> Ibn `Abbas, may Allah be pleased with him, and others said; "In disbelief and doubt, they are heedless and playful."<span class="taf-foot"><span class="n">8</span><span class="t">At-Tabari 22:400-401</span></span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Who are under a cover of Sahun</span> Ibn `Abbas, may Allah be pleased with him, and others said; "In disbelief and doubt, they are heedless and playful."<span class="taf-foot"><span class="t">At-Tabari 22:400-401</span></span>
 <span class="taf-ay-end"></span>
 '),
 (1000, 51, 12, '
@@ -99,18 +99,94 @@ Allah said,
 (1000, 51, 13, '
 Allah the Exalted replied,
 <span class="taf-ref-3 taf-arb taf-qur">(۱۳) يوم هم على النار يفتنون</span>
-<span class="taf-ref-3 taf-tr taf-tr-qur">(It will be) a Day when they will be Yuftanun in the Fire</span> Ibn `Abbas, Mujahid, Al-Hasan and several others said that Yuftanun means punished.<span class="taf-foot"><span class="n">9</span><span class="t">At-Tabari 22:402</span></span> Mujahid said: "Just as gold is forged in the fire"<span class="taf-foot"><span class="n">10</span><span class="t">At-Tabari 22:402</span></span> A group of others also including Mujahid, Ikrimah, Ibrahim An-Nakha\`i, Zayd bin Aslam, and Sufyan Ath-Thawri said, "They will be burnt."
+<span class="taf-ref-3 taf-tr taf-tr-qur">(It will be) a Day when they will be Yuftanun in the Fire</span> Ibn `Abbas, Mujahid, Al-Hasan and several others said that Yuftanun means punished.<span class="taf-foot"><span class="t">At-Tabari 22:402</span></span> Mujahid said: "Just as gold is forged in the fire"<span class="taf-foot"><span class="t">At-Tabari 22:402</span></span> A group of others also including Mujahid, Ikrimah, Ibrahim An-Nakha\`i, Zayd bin Aslam, and Sufyan Ath-Thawri said, "They will be burnt."
 <span class="taf-ay-end"></span>
 '),
 (1000, 51, 14, '
 Allah the Exalted replied,
 <span class="taf-ref-3 taf-arb taf-qur">ذوقوا فتنتكم</span>
-<span class="taf-ref-3 taf-tr taf-tr-qur">Taste you your trial!</span> Mujahid said, "Your burning"<span class="taf-foot"><span class="n">11</span><span class="t">At-Tabari 22:403</span></span> while others said, "Your punishment."
-<span class="taf-ref-3 taf-arb taf-qur">هذا الذي كنتم به تستعجلون</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Taste you your trial!</span> Mujahid said, "Your burning"<span class="taf-foot"><span class="t">At-Tabari 22:403</span></span> while others said, "Your punishment."
+<br/>
+<span class="taf-ref-3 taf-arb taf-qur">(۱۴) هذا الذي كنتم به تستعجلون</span>
 <span class="taf-ref-3 taf-tr taf-tr-qur">This is what you used to ask to be hastened!</span> This will be said admonishing, chastising, humiliating and belittling them. Allah knows best.
 <span class="taf-ay-end"></span>
+'),
+(1000, 51, 15, '
+<span class="taf-ref-3 taf-arb taf-qur">(۱۵) إن المتقين في جنات وعيون</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Verily, those who have Taqwa will be in the midst of Gardens and Springs,</span> Allah the Exalted informs about those who have Taqwa, hat on the Day of their Return they will be amidst gardens and springs. To the contrary the miserable ones will be amidst torment, punishment, fire and chains.
+<span class="taf-ay-end"></span>
+'),
+(1000, 51, 16, '
+<span class="taf-ref-3 taf-arb taf-qur">آخذين ما آتاهم ربهم</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Taking joy in the things which their Lord has given them.</span>
+<br/>
+His statement;
+<span class="taf-ref-3 taf-arb taf-qur">آخذين</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Taking</span> describes the state of the pople of Taqwa in the midst of gardens and springs. They will receive what their Lord gives them, meaning, delight, happiness and favors. Allah the Exalted and Most Honored said,
+<span class="taf-ref-3 taf-arb taf-qur">إنهم كانوا قبل ذلك</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Verily, they were before that</span>, in the life of the world,
+<span class="taf-ref-3 taf-arb taf-qur">محسنين</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">gooddoers</span> As He said:
+<span class="taf-ref-3 taf-arb taf-qur">(۲۴) كلوا واشربوا هنيئا بما أسلفتم في الأيام الخالي</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Eat and drink at ease for that which you have sent on before you in days past! (<a class="footnote-qref-link">69:24</a>)</span>
+<span class="taf-ay-end"></span>
+'),
+(1000, 51, 17, '
+Allah the Exalted and Most Honored said,
+<span class="taf-ref-3 taf-arb taf-qur">(۱۷) كانوا قليلا من الليل ما يهجعون</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">They used to sleep but little at night.</span> The scholars of Tafsir have two opinions about this:
+<br/>
+<span class="taf-head">The First Opinion</span>
+The first is that, they used to spend a little part of every night awake. Ibn `Abbas said, "Every night, they would worship Allah, even during a little part of the night."<span class="taf-foot"><span class="t">At-Tabari 22:407 </span></span> Qatadah narrated that Mutarrif bin `Abdullah said, "Hardly a night would pass by them that they did not pray to Allah the Exalted and Most Honored, either in the beginning, or the middle of it."<span class="taf-foot"><span class="t">At-Tabari 22:407</span></span> Mujahid said, "Only a few nights, if any, would they sleep through the night until the morning without praying Tahajjud."<span class="taf-foot"><span class="t">At-Tabari 22:408</span></span> Qatadah said similarly.<span class="taf-foot"><span class="t">At-Tabari 22:408</span></span> Anas bin Malik and Abu Al-`Aliyah said, "They used to pray between Al-Maghrib and Al-`Isha`."<span class="taf-foot"><span class="t">At-Tabari 22:407-408</span></span> 
+<br/>
+<span class="taf-head">The Second Opinion</span>
+They used to spend a little part of the night in sleep. This was preferred by Ibn Jarir. Al-Hasan Al Basri said:
+<span class="taf-ref-3 taf-arb taf-qur">(۱۷) كانوا قليلا من الليل ما يهجعون</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">They used to sleep but little at night.</span> "They performed voluntary night prayer and would not sleep during the night except a little. They were active and would continue until they were seeking forgiveness right before dawn"<span class="taf-foot"><span class="t">At-Tabari 22:408-409</span></span>
+<br/>
+`Abdullah bin Salam said, "When the Messenger of Allah ﷺ arrived at Al-Madinah, people quickly gathered around him and I was among them. When I saw his face, I knew that it was not the face of a liar. The first statement I heard from him was,
+<span class="taf-ref-3 taf-tr taf-tr-had">O people! Feed with food, keep relations to kith and kin, spread the Salam, pray at night while people are asleep, and you will enter Paradise in peace</span>"<span class="taf-foot"><span class="t">Tuhufat Al-Ahwadhi 7:187</span></span>
+<br/>
+Imam Ahmad recorded that `Abdullah bin `Umar said that the Messenger of Allah ﷺ said, 
+<span class="taf-ref-3 taf-tr taf-tr-had">For those who use soft speech, feed food and spend the night in voluntary prayer while people are asleep</span><span class="taf-foot"><span class="t">Ahmed 2:173</span></span>
+<span class="taf-ay-end"></span>
+'),
+(1000, 51, 18, '
+<span class="taf-ref-3 taf-arb taf-qur">(وبالأسحار هم يستغفرون (۱۸</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">And in the hours before dawn, they were asking for forgiveness.</span> Mujahid and several others said: "They were performing Salah."<span class="taf-foot"><span class="t">At-Tabari 22:413</span></span> Others said that they would stand in prayer during the night and delayed asking Allah for forgiveness utnil latter hours before dawn. As Allah, the Exalted the Blessed, said;
+<span class="taf-ref-3 taf-arb taf-qur">(والمستغفرين بالأسحار (۱۷</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">And those who seek forgiveness during the last hours of the night. (<a class="footnote-qref-link">3:17</a>)</span>
+This is because it is better if asking for forgiveness is done while praying.
+<br/>
+It is confirmed in the Sahih collections as well as others, from several Companions, that the Messenger of Allah ﷺ said,
+<span class="taf-ref-3 taf-tr taf-tr-had">Allah, the Most High, descends each night to the lowest heaven when the last third of the night remains. He says, "Is there anyone who is repending so that I may accept his repentance? Is there anyone seeking forgiveness, so that I may forgive him? Is there anyone asking of Me, so that I may grant him his request?" until Fajr begins.</span><span class="taf-foot"><span class="t">Fath Al-Bari 3:35, 11:133 and 13:473<br/>Muslim 1:521 and 523<br/>Abu Dawud 2:77 and 5:101<br/>Tuhfat Al-Ahwadhi 9:471<br/>An-Nasa\'i in Al-Kubra 4:24<br/>Ibn Majah 1:435</span></span>
+<br/>
+Many of the scholars of Tafsir said that when the Prophet Yaqub said to his sons:
+<span class="taf-ref-3 taf-arb taf-qur">سوف أستغفر لكم ربي</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">I will ask my Lord for forgiveness for you (<a class="footnote-qref-link">12:98</a>)</span> he delayed doing so until the hours before dawn.
+<span class="taf-ay-end"></span>
+'),
+(1000, 51, 19, '
+<span class="taf-ref-3 taf-arb taf-qur">(۱۹) وفي أموالهم حق للسائل والمحروم</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">And in their wealth there was the right of the Sa\'il and the Mahrum.</span> After Allah mentioned their quality of performing prayer, He then mentioned their quality of spending in charity and acts of compassion and kindness,
+<span class="taf-ref-3 taf-arb taf-qur">وفي أموالهم حق</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">And in their wealth there was the right</span> a designated part which they dedicated to the Sa\'il and Mahrum. The Sa\'il is the poor who begs others, and he has a due right. As for the Mahrum, Ibn \`Abbas and Mujahid said, "He is the poor person who does not receive a stipend."<span class="taf-foot"><span class="t">At-Tabari 22:414</span></span> Meaning he does not receive a stipend from the Muslim treasury, nor does he have a means of income, nor a profession.
+<br/>
+The Mother of the faithful, \`Aishah may Allah be pleased with her, said about the Mahrum, "He is the displaced, the one who does not have a profession to easily earn an income from." Qatadah and Az-Zuri: "The Mahrum is the one who does not ask the people for anything"<span class="taf-foot"><span class="t">At-Tabari 22:416</span></span> Az-Zuhri added that the Messenger of Allah ﷺ said,
+<span class="taf-ref-3 taf-tr taf-tr-had">The poor (Miskin) is not the one who goes round to the people and asks them for a mouthful or two or a date or two. But the poor is the one who does not have enough to satisfy his needs and whose condition is not known to others, so that others may give him something in charity.</span><span class="taf-foot"><span class="t">An-Nasa\'i 5:85</span></span> This Hadith is recorded in the Two Sahihs using another chain of narration.<span class="taf-foot"><span class="t">Fath Al-Bari 3:399<br/>Muslim 2:719</span></span>
+<span class="taf-ay-end"></span>
+'),
+(1000, 51, 20, '
+<span class="taf-ref-3 taf-arb taf-qur">(۲۰) وفي الأرض آيات للموقنين</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">And on the earth are signs for those who have faith with certainty.</span> Allah says that there are signs on earth that testify to the might of the Creator and His boundless ability. These signs include what Allah placed on the earth, the various plants, animals, valleys, mountains, deserts, rivers and oceans. He also created mankind with different languages, colors, intentions and abilities, and a variety among them, differences in the power of understanding and comprehension, their deeds, and ultimately earning happiness or misery.
+<span class="taf-ay-end"></span>
+'),
+(1000, 51, 21, '
+Allah put every organ in their bodies in its rightful place where they most need it to be.
+<span class="taf-ref-3 taf-arb taf-qur">(۲۱) وفي أنفسكم ۚ أفلا تبصرون</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">And also in yourselves. Will you not then see?</span> Qatadah commented, "He who thinks about his own creation will realize that he was created with flexible joints so that it is easy for him to perform acts of worship."<span class="taf-foot"><span class="t">Al-Qurtubi 17:40</span></span>
+<span class="taf-ay-end"></span>
 ')
-
 
 
 
