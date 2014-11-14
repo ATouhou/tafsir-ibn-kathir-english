9dc65ec9303c5de582f1e6e3ddb58d52758f1064
mkhan3189 <mkhan3189@gmail.com>
2014-11-14 15:57:09 +1100

    foot notes and references

--- sqls/051.sql
+++ sqls/051.sql
@@ -7,9 +7,9 @@ INSERT INTO `Quran`
   (`DatabaseID`, `SuraID`, `VerseID`, `AyahText`)
 VALUES
 (1000, 51, 1, '
-The Commander of the faithful, Ali ibn Abi Talib may Allah be pleased with him, ascended the Minbar in Kufah and declared, "Any Ayah in the Book of Allah the Exalted and any Sunnah from Allah\'s Messenger ﷺ you ask me about today, I will explain them." Ibn Al-Kawwa stood up and said, "O Leader of the faithful! What is the meaning of Allah\'s statement,
+The Commander of the faithful, \`Ali ibn Abi Talib may Allah be pleased with him, ascended the Minbar in Kufah and declared, "Any Ayah in the Book of Allah the Exalted and any Sunnah from Allah\'s Messenger ﷺ you ask me about today, I will explain them." Ibn Al-Kawwa stood up and said, "O Leader of the faithful! What is the meaning of Allah\'s statement,
 <span class="taf-ref-3 taf-arb taf-qur">(۱) والذاريات ذروا </span>
-<span class="taf-ref-3 taf-tr taf-tr-qur">By the scattering Dhariyat</span>?" and Ali said, "The wind."
+<span class="taf-ref-3 taf-tr taf-tr-qur">By the scattering Dhariyat</span>?" and \`Ali said, "The wind."
 <span class="taf-ay-end"></span>
 '),
 (1000, 51, 2, '
@@ -21,37 +21,71 @@ The man asked,
 (1000, 51, 3, '
 The man asked, 
 <span class="taf-ref-3 taf-arb taf-qur">(۳) فالجاريات يسرا</span>
-<span class="taf-ref-3 taf-tr taf-tr-qur">And the steady Jariyaat</span>?" and Ali said, "The ships."
+<span class="taf-ref-3 taf-tr taf-tr-qur">And the steady Jariyaat</span>?" and \`Ali said, "The ships."
 <span class="taf-ay-end"></span>
 '),
 (1000, 51, 4, '
 The man asked, 
 <span class="taf-ref-3 taf-arb taf-qur">(۴)  فالمقسمات أمرا</span>
-<span class="taf-ref-3 taf-tr taf-tr-qur">And the distributors of command</span>?" and Ali said, it refers to "The angels."
+<span class="taf-ref-3 taf-tr taf-tr-qur">And the distributors of command</span>?" and Ali said, it refers to "The angels."<span class="taf-foot"><span class="n">1</span><span class="t">At-Tabari 22:389-392 and `Abdur-Razzaq 3:41</span></span>
 <br>Some scholars said that <span class="taf-tri">Al-Jariyat Yusra</span> refers to the stars that float in their orbits with ease. This would mean that the things mentioned were ascendant in their order, beginning with the lower, then mentioning the higher one after that, etc. The winds being the clouds, the stars are above them and the angels who distribute by Allah\'s order are above that, and they descend with Allah\'s legislative orders and the decrees He determines.
 <span class="taf-ay-end"></span>
 '),
 (1000, 51, 5, '
 These Ayat contain a vow from Allah that Resurrection shall come to pass. Allah\'s statement
 <span class="taf-ref-3 taf-arb taf-qur">(۵) إنما توعدون لصادق</span>
-<span class="taf-ref-3 taf-tr taf-tr-qur">Verily, that which you are promised is surely true.</span>, it is a truthful promise
+<span class="taf-ref-3 taf-tr taf-tr-qur">Verily, that which you are promised is surely true.</span>  it is a truthful promise
 <span class="taf-ay-end"></span>
 '),
 (1000, 51, 6, '
 <span class="taf-ref-3 taf-arb taf-qur">وإن الدين</span>
 <span class="taf-ref-3 taf-tr taf-tr-qur">And verily, Ad-Din</span> the Recompense,
 <span class="taf-ref-3 taf-arb taf-qur">لواقع</span>
-<span class="taf-ref-3 taf-tr taf-tr-qur">will occur</span>, it will surely come to pass.
+<span class="taf-ref-3 taf-tr taf-tr-qur">will occur</span>  it will surely come to pass.
 <span class="taf-ay-end"></span>
 '),
 (1000, 51, 7, '
 Then Allah the Exalted said,
 <span class="taf-ref-3 taf-arb taf-qur">(۷) والسماء ذات الحبك</span>
-<span class="taf-ref-3 taf-tr taf-tr-qur">By the heaven full of Hubuk</span> Ibn \'Abbas said; "Full of beauty, grace, magnificence and perfection." Mujahid, \'Ikrimah, Sa\'d bin Jubayr, Abu Malik, Abu Salih, As Suddi, Qatadah, \'Atiyah Al-\'Awfi, Ar-Rabi\' bin Anas and other said similarly. Ad-Dahhak, Al-Minhal bin \'Amr and others said, "The meandering of the water, sand and plants when the wind passes over them; carving paths out of them, that is the Hubuk."
+<span class="taf-ref-3 taf-tr taf-tr-qur">By the heaven full of Hubuk</span> Ibn \`Abbas said; "Full of beauty, grace, magnificence and perfection."<span class="taf-foot"><span class="n">2</span><span class="t">At-Tabari 22:395-396</span></span> Mujahid, \`Ikrimah, Sa\'d bin Jubayr, Abu Malik, Abu Salih, As Suddi, Qatadah, \`Atiyah Al-\`Awfi, Ar-Rabi\` bin Anas and other said similarly.<span class="taf-foot"><span class="n">3</span><span class="t">At-Tabari 22:396-397</span></span> Ad-Dahhak, Al-Minhal bin \`Amr and others said, "The meandering of the water, sand and plants when the wind passes over them; carving paths out of them, that is the Hubuk."
 <br/>
 All of these sayings return to the same meaning, that of beauty and complexity. The sky is high above us, clear yet think, firmly structured, spacious and graceful, beautified with stars such as the sun and orbiting planets such as the moon and the planets of the solar system.
 <span class="taf-ay-end"></span>
+'),
+(1000, 51, 8, '
+Allah the Exalted said,
+<span class="taf-ref-3 taf-arb taf-qur">(۸) إنكم لفي قول مختلف</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Certainly, you have different ideas</span> Allah says, \'you disbelievers who deny the Messengers have different and confused opinions that do not connect or conform to each other.\' Qatadah commented on the Ayah, "You have different ideas about that Qur\'an. Some of you agree that it is true while some others deny this fact."<span class="taf-foot"><span class="n">4</span><span class="t">`Abdur-Razzaq 4:242</span></span>
+<span class="taf-ay-end"></span>
+'),
+(1000, 51, 9, '
+Allah the Exalted said,
+<span class="taf-ref-3 taf-arb taf-qur">(۹) يؤفك عنه من أفك</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Turned aside therefrom is he who is turned aside.</span> Allah says, these confused and different opinions only fool those who are inwardly misguided. Surely, such falsehood is accepted, embraced and it becomes the source of confusion only for those who are misguided and originally liars, the fools who have no sound comprehension, as Allah said,
+<span class="taf-ref-3 taf-arb taf-qur">(فإنكم وما تعبدون (۱٦۱)  ما أنتم عليه بفاتنين (۱٦۲)  إلا من هو صال الجحيم (۱٦۳</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">So, verify you and those whom you worship cannot lead astray, except those who are predetermined to burn in Hell! (<a class="footnote-qref-link">37:161-163</a>)</span>
+<br/>
+Ibn \`Abbas, may Allah be pleased with him, and As-Suddi said:
+<span class="taf-ref-3 taf-arb taf-qur">(۹) يؤفك عنه من أفك</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Turned aside therefrom is he who is turned aside.</span> "He who is misguided is led astray from it."
+<span class="taf-ay-end"></span>
+'),
+(1000, 51, 10, '
+Allah said,
+<span class="taf-ref-3 taf-arb taf-qur">(۱۰) قتل الخراصون</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Cursed be Al-Kharrasun</span>  Mujahid said, "The liars. This is similar to what is mentioned in (Surah) \`Abasa:"
+<span class="taf-ref-3 taf-arb taf-qur">(۱۷) قتل الإنسان ما أكفره</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Be cursed man! How ungrateful he is! (<a class="footnote-qref-link">80:17</a>)</span>
+<br/>
+Al-Kharrasun are those who claim that they will never be brought back to life, doubting the coming of Resurrection."<span class="taf-foot"><span class="n">5</span><span class="t">At-Tabari 22:400</span></span>
+<br/>
+\`Ali bin Abi Talhah reported from Ibn \`Abbas;
+<span class="taf-ref-3 taf-arb taf-qur">(۱۰) قتل الخراصون</span>
+<span class="taf-ref-3 taf-tr taf-tr-qur">Cursed be Al-Kharrasun</span>  "Cursed be the doubters."<span class="taf-foot"><span class="n">6</span><span class="t">At-Tabari 22:400</span></span> Mu\`adh said similarly, may Allah be pleased with him. During one of his speeches he said, "Destroyed be the doubters." Qatadah said, "Al-Kharrasun are the pople of doubt and suspicion."<span class="taf-foot"><span class="n">7</span><span class="t">At-Tabari 22:400-401</span></span>
+<span class="taf-ay-end"></span>
 ')
+
+
 
 
 
